afecda5b660081ec7c245d5b1532ffa0873cb090
safir2024 <safir2024@DESKTOP-C99DU39>
2025-10-21 10:53:20 +0330

    Add project files.

---
+++ ConsoleApplication7/ConsoleApplication7.cpp
@@ -0,0 +1,26 @@
+#include<iostream>        
+#include<iomanip>     //2x+5y=90
+#include<string>
+using namespace std;
+void main()
+{
+	int counter = 0, i, j;
+	for (i =0;i <= 45;i++)
+	{
+		counter++;
+		for (j = 0;j <= 18;j++)
+		{
+			counter++;
+			if (2 * i + 5 * j == 90)
+			{
+				counter++;
+			}
+			counter++;
+		}
+		counter++;
+	}
+	counter++;
+	cout << counter;
+}
+
+
